dcd0cba0ce171960336253591b876cb71dbd57bb
Cova <Cova@DESKTOP-4E120RL>
2017-10-11 17:52:16 +0200

    create director test

---
+++ src/test/java/apaw/DirectorResourceFunctionalTesting.java
@@ -0,0 +1,48 @@
+package apaw;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import apaw.api.daos.DaoFactory;
+import apaw.api.resources.DirectorResource;
+import apaw.http.HttpClientService;
+import apaw.http.HttpMethod;
+import apaw.http.HttpRequest;
+import apaw.http.HttpRequestBuilder;
+import apaw.http.HttpException;
+import apaw.api.daos.memory.DaoMemoryFactory;
+
+public class DirectorResourceFunctionalTesting {
+    
+    
+    @Before
+    public void before(){
+        DaoFactory.setFactory(new DaoMemoryFactory());
+    }
+    
+
+    private void createDirector() {
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(DirectorResource.DIRECTORS).body("1").build();
+        new HttpClientService().httpRequest(request);
+    }
+    
+    @Test
+    public void testCreateDirector() {
+        this.createDirector();
+    }
+    
+    @Test(expected = HttpException.class)
+    public void testCreateDirectorIdEmpty() {
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(DirectorResource.DIRECTORS).body("").build();
+        new HttpClientService().httpRequest(request);
+    }
+    
+    @Test(expected = HttpException.class)
+    public void testCreateWithoutDirector() {
+        HttpRequest request = new HttpRequestBuilder().method(HttpMethod.POST).path(DirectorResource.DIRECTORS).build();
+        new HttpClientService().httpRequest(request);
+    }
+    
+    
+    
+}
